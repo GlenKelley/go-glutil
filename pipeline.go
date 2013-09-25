c2f4922fc2d532f805fb94b8c002eb032e22af72
Glen Kelley <glen.t.kelley@gmail.com>
2013-09-25 23:13:58 +1000

    working but needs cleanup

--- pipeline.go
+++ pipeline.go
@@ -2,9 +2,12 @@ package render
 
 import (
     "os"
+    "fmt"
     "image"
+    "errors"
     "unsafe"
     "reflect"
+    "io/ioutil"
     _ "image/png"
     _ "image/jpeg"
     gl "github.com/GlenKelley/go-gl32"
@@ -52,14 +55,17 @@ func ImageData(img image.Image) (gl.Sizei, gl.Sizei, gl.Enum, gl.Enum, gl.Pointe
 
 //gl.Sizei(width), gl.Sizei(height)
 //gl.RGB, gl.UNSIGNED_BYTE
-func LoadTexture(filename string) gl.Uint {
+func LoadTexture(filename string) (gl.Uint, error) {
     var texture gl.Uint
     file, err := os.Open(filename)
     if err != nil {
-        panic(err)
+        return 0, err
     }
     defer file.Close()
     img, _, err := image.Decode(file)
+    if err != nil {
+        return 0, err
+    }
     width, height, format, channelType, pixels := ImageData(img)
     gl.GenTextures(1, &texture)
     gl.BindTexture(gl.TEXTURE_2D, texture)
@@ -71,8 +77,42 @@ func LoadTexture(filename string) gl.Uint {
           gl.TEXTURE_2D, 0,           /* target, level of detail */
           gl.RGB8,                    /* internal format */
           width, height, 0,           /* width, height, border */
-          format, channelType,   /* external format, type */
+          format, channelType,          /* external format, type */
           pixels,                      /* pixels */
     )
-    return texture
+    return texture, nil
+}
+
+func LoadShader(shaderType gl.Enum, filename string) (gl.Uint, error){
+    bytes, err := ioutil.ReadFile(filename)
+    if err != nil {
+        return 0, err
+    }
+    shader := gl.CreateShader(shaderType)
+    source := string(bytes)
+    gl.ShaderSource(shader, []string{source})
+    gl.CompileShader(shader)
+    var ok gl.Int
+    gl.GetShaderiv(shader, gl.COMPILE_STATUS, &ok)
+    if (ok == 0) {
+        fmt.Println(gl.GetShaderInfoLog(shader))
+        gl.DeleteShader(shader)
+        return 0, errors.New("Failed to compile " + filename + "\n")
+    }
+    return shader, nil
+}
+
+func CreateProgram(vertexShader, fragmentShader gl.Uint) (gl.Uint, error) {
+    program := gl.CreateProgram()
+    gl.AttachShader(program, vertexShader)
+    gl.AttachShader(program, fragmentShader)
+    gl.LinkProgram(program)
+    var ok gl.Int
+    gl.GetProgramiv(program, gl.LINK_STATUS, &ok)
+     if (ok == 0) {
+         fmt.Println(gl.GetProgramInfoLog(program))
+         gl.DeleteProgram(program)
+         return 0, errors.New("Failed to link shader program")
+     }
+     return program, nil
 }
