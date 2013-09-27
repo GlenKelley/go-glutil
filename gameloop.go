162e99cceb65f5fbfb86e44878d93f7c1850b1d4
Glen Kelley <glen.t.kelley@gmail.com>
2013-09-27 15:36:10 +1000

    go fmt

--- gameloop.go
+++ gameloop.go
@@ -1,98 +1,98 @@
 package render
 
 import (
-    "time"
-    "errors"
-    glfw "github.com/go-gl/glfw3"
+	"errors"
+	glfw "github.com/go-gl/glfw3"
+	"time"
 )
 
 type WindowDelegate interface {
-    Init(window *glfw.Window)
-    Draw(window *glfw.Window)
-    Reshape(window *glfw.Window, width, height int)
-    MouseClick(window *glfw.Window, button glfw.MouseButton, action glfw.Action, mod glfw.ModifierKey)
-    MouseMove(window *glfw.Window, xpos float64, ypos float64)
-    KeyPress(window *glfw.Window, k glfw.Key, s int, action glfw.Action, mods glfw.ModifierKey)
-    Scroll(window *glfw.Window, xoff float64, yoff float64)
-    Simulate(now time.Time, elapsed time.Duration, duration time.Duration)
-    OnClose(window *glfw.Window)
-    IsIdle() bool
-    NeedsRender() bool
+	Init(window *glfw.Window)
+	Draw(window *glfw.Window)
+	Reshape(window *glfw.Window, width, height int)
+	MouseClick(window *glfw.Window, button glfw.MouseButton, action glfw.Action, mod glfw.ModifierKey)
+	MouseMove(window *glfw.Window, xpos float64, ypos float64)
+	KeyPress(window *glfw.Window, k glfw.Key, s int, action glfw.Action, mods glfw.ModifierKey)
+	Scroll(window *glfw.Window, xoff float64, yoff float64)
+	Simulate(now time.Time, elapsed time.Duration, duration time.Duration)
+	OnClose(window *glfw.Window)
+	IsIdle() bool
+	NeedsRender() bool
 }
 
 func WindowAspectRatio(window *glfw.Window) float64 {
-    frameWidth, frameHeight := window.GetFramebufferSize()
-    return float64(frameWidth) / float64(frameHeight)
+	frameWidth, frameHeight := window.GetFramebufferSize()
+	return float64(frameWidth) / float64(frameHeight)
 }
 
 func bindEvents(window *glfw.Window, delegate WindowDelegate) {
-    window.SetFramebufferSizeCallback(delegate.Reshape)
-    window.SetMouseButtonCallback(delegate.MouseClick)
-    window.SetCursorPositionCallback(delegate.MouseMove)
-    window.SetKeyCallback(delegate.KeyPress)
-    window.SetScrollCallback(delegate.Scroll)
-    window.SetCloseCallback(delegate.OnClose)
+	window.SetFramebufferSizeCallback(delegate.Reshape)
+	window.SetMouseButtonCallback(delegate.MouseClick)
+	window.SetCursorPositionCallback(delegate.MouseMove)
+	window.SetKeyCallback(delegate.KeyPress)
+	window.SetScrollCallback(delegate.Scroll)
+	window.SetCloseCallback(delegate.OnClose)
 }
 
 func CreateWindow(width, height int, name string, fullscreen bool, delegate WindowDelegate) error {
-    if !glfw.Init() {
-        return errors.New("Failed to initialize GLFW")
-    }
-    defer glfw.Terminate()
-        
-    glfw.WindowHint(glfw.DepthBits, 16)
-    glfw.WindowHint(glfw.ContextVersionMajor, 3)
-    glfw.WindowHint(glfw.ContextVersionMinor, 2)
-    glfw.WindowHint(glfw.OpenglProfile, glfw.OpenglCoreProfile)
-    glfw.WindowHint(glfw.OpenglForwardCompatible, 1)
+	if !glfw.Init() {
+		return errors.New("Failed to initialize GLFW")
+	}
+	defer glfw.Terminate()
 
-    var monitor *glfw.Monitor = nil
-    var err error = nil
-    if fullscreen {
-        monitor, err = glfw.GetPrimaryMonitor()
-        if err != nil {
-            return err
-        }
-        
-        vidModes, _ := monitor.GetVideoModes()
-        maxResolution := vidModes[len(vidModes)-1]
-        width = maxResolution.Width
-        height = maxResolution.Height
-    }
-    window, err := glfw.CreateWindow(width, height, name, monitor, nil)
-    if err != nil {
-        return err
-    }
-    
-    if fullscreen {
-        window.SetInputMode(glfw.Cursor, glfw.CursorDisabled)        
-    }
+	glfw.WindowHint(glfw.DepthBits, 16)
+	glfw.WindowHint(glfw.ContextVersionMajor, 3)
+	glfw.WindowHint(glfw.ContextVersionMinor, 2)
+	glfw.WindowHint(glfw.OpenglProfile, glfw.OpenglCoreProfile)
+	glfw.WindowHint(glfw.OpenglForwardCompatible, 1)
 
-    bindEvents(window, delegate)
-    
-    window.MakeContextCurrent()
-    glfw.SwapInterval(1)
+	var monitor *glfw.Monitor = nil
+	var err error = nil
+	if fullscreen {
+		monitor, err = glfw.GetPrimaryMonitor()
+		if err != nil {
+			return err
+		}
 
-    delegate.Init(window)
-    frameWidth, frameHeight := window.GetFramebufferSize()
-    delegate.Reshape(window, frameWidth, frameHeight)
-    start := time.Now()
-    last := start
-    for !window.ShouldClose() {
-        now := time.Now()
-        duration := now.Sub(last)
-        elapsed := now.Sub(start)
-        delegate.Simulate(now, elapsed, duration)
-        last = now
-        if delegate.NeedsRender() {
-            delegate.Draw(window)
-        }
-        window.SwapBuffers()
-        if delegate.IsIdle() {
-            glfw.WaitEvents()            
-        } else {
-            glfw.PollEvents()
-        }
-    }
-    return nil
+		vidModes, _ := monitor.GetVideoModes()
+		maxResolution := vidModes[len(vidModes)-1]
+		width = maxResolution.Width
+		height = maxResolution.Height
+	}
+	window, err := glfw.CreateWindow(width, height, name, monitor, nil)
+	if err != nil {
+		return err
+	}
+
+	if fullscreen {
+		window.SetInputMode(glfw.Cursor, glfw.CursorDisabled)
+	}
+
+	bindEvents(window, delegate)
+
+	window.MakeContextCurrent()
+	glfw.SwapInterval(1)
+
+	delegate.Init(window)
+	frameWidth, frameHeight := window.GetFramebufferSize()
+	delegate.Reshape(window, frameWidth, frameHeight)
+	start := time.Now()
+	last := start
+	for !window.ShouldClose() {
+		now := time.Now()
+		duration := now.Sub(last)
+		elapsed := now.Sub(start)
+		delegate.Simulate(now, elapsed, duration)
+		last = now
+		if delegate.NeedsRender() {
+			delegate.Draw(window)
+		}
+		window.SwapBuffers()
+		if delegate.IsIdle() {
+			glfw.WaitEvents()
+		} else {
+			glfw.PollEvents()
+		}
+	}
+	return nil
 }
--- pipeline.go
+++ pipeline.go
@@ -1,39 +1,41 @@
 package render
 
 import (
-    "os"
-    "fmt"
-    "image"
-    "errors"
-    "runtime/debug"
-    "unsafe"
-    "reflect"
-    "io/ioutil"
-    _ "image/png"
-    _ "image/jpeg"
-    gl "github.com/GlenKelley/go-gl32"
+   "errors"
+   "fmt"
+   gl "github.com/GlenKelley/go-gl32"
+   "image"
+   _ "image/jpeg"
+   _ "image/png"
+   "io/ioutil"
+   "os"
+   "reflect"
+   "runtime/debug"
+   "unsafe"
 )
 
 type ShaderLibrary struct {
    FragmentShaders map[string]gl.FragmentShader
-   VertexShaders map[string]gl.VertexShader
-   Programs map[string]gl.Program
+   VertexShaders   map[string]gl.VertexShader
+   Programs        map[string]gl.Program
 }
 
 func NewShaderLibrary() ShaderLibrary {
-   return ShaderLibrary {
+   return ShaderLibrary{
       make(map[string]gl.FragmentShader),
       make(map[string]gl.VertexShader),
       make(map[string]gl.Program),
    }
 }
 
-func (lib *ShaderLibrary) LoadFragmentShader(tag ,filename string) {   
+func (lib *ShaderLibrary) LoadFragmentShader(tag, filename string) {
    _, ok := lib.FragmentShaders[tag]
    if !ok {
       shader := gl.FragmentShader(gl.CreateShader(gl.FRAGMENT_SHADER))
       err := LoadFragmentShaderSource(shader, filename)
-      if err != nil {panic(err)}
+      if err != nil {
+         panic(err)
+      }
       lib.FragmentShaders[tag] = shader
    }
 }
@@ -43,7 +45,9 @@ func (lib *ShaderLibrary) LoadVertexShader(tag, filename string) {
    if !ok {
       shader := gl.VertexShader(gl.CreateShader(gl.VERTEX_SHADER))
       err := LoadVertexShaderSource(shader, filename)
-      if err != nil {panic(err)}
+      if err != nil {
+         panic(err)
+      }
       lib.VertexShaders[tag] = shader
    }
 }
@@ -55,7 +59,9 @@ func (lib *ShaderLibrary) LoadProgram(tag, vsfilename, fsfilename string) {
    lib.LoadFragmentShader(ftag, fsfilename)
    program := gl.CreateProgram()
    err := LoadProgram(program, lib.VertexShaders[vtag], lib.FragmentShaders[ftag])
-   if err != nil {panic(err)}
+   if err != nil {
+      panic(err)
+   }
    _, ok := lib.Programs[tag]
    if !ok {
       lib.Programs[tag] = program
@@ -67,7 +73,7 @@ func (lib *ShaderLibrary) LoadProgram(tag, vsfilename, fsfilename string) {
 func (lib *ShaderLibrary) BindProgramLocations(tag string, obj interface{}) {
    program, ok := lib.GetProgram(tag)
    if ok {
-      BindProgramLocations(program, obj)      
+      BindProgramLocations(program, obj)
    }
 }
 
@@ -84,246 +90,241 @@ func (lib *ShaderLibrary) GetProgram(tag string) (gl.Program, bool) {
 }
 
 func ArrayPtr(data interface{}) (gl.Pointer, gl.Sizeiptr) {
-    var size gl.Sizeiptr
-    var ptr gl.Pointer 
-    switch data := data.(type) {
-    case []float64:
-        duplicate := make([]gl.Float, len(data))
-        for i, v := range data {
-            duplicate[i] = gl.Float(v)
-        }
-        ptr, size = ArrayPtr(duplicate)
-    case []float32:
-        if len(data) == 0 {
-            size = 0
-            ptr = gl.Pointer(nil)
-        } else {
-            var v float32
-            size = gl.Sizeiptr(len(data) * int(unsafe.Sizeof(v)))
-            ptr = gl.Pointer(&data[0])
-        }
-    case []gl.Float:
-        if len(data) == 0 {
-            size = 0
-            ptr = gl.Pointer(nil)
-        } else {
-            var v gl.Float
-            size = gl.Sizeiptr(len(data) * int(unsafe.Sizeof(v)))
-            ptr = gl.Pointer(&data[0])
-        }
-    case []int16:
-        if len(data) == 0 {
-            size = 0
-            ptr = gl.Pointer(nil)
-        } else {
-            var v int16
-            size = gl.Sizeiptr(len(data) * int(unsafe.Sizeof(v)))
-            ptr = gl.Pointer(&data[0])
-        }
-    case []gl.Ushort:
-        if len(data) == 0 {
-            size = 0
-            ptr = gl.Pointer(nil)
-        } else {
-            var v gl.Ushort
-            size = gl.Sizeiptr(len(data) * int(unsafe.Sizeof(v)))
-            ptr = gl.Pointer(&data[0])
-        }
-    case []int:
-        duplicate := make([]gl.Ushort, len(data))
-        for i, v := range data {
-            duplicate[i] = gl.Ushort(v)
-        }
-        ptr, size = ArrayPtr(duplicate)
-    case []gl.Int:
-        if len(data) == 0 {
-            size = 0
-            ptr = gl.Pointer(nil)
-        } else {
-            var v gl.Int
-            size = gl.Sizeiptr(len(data) * int(unsafe.Sizeof(v)))
-            ptr = gl.Pointer(&data[0])
-        }
-    default:
-        panic(fmt.Sprintln("unknown data type:", reflect.TypeOf(data)))
-    }
-    return ptr, size
+   var size gl.Sizeiptr
+   var ptr gl.Pointer
+   switch data := data.(type) {
+   case []float64:
+      duplicate := make([]gl.Float, len(data))
+      for i, v := range data {
+         duplicate[i] = gl.Float(v)
+      }
+      ptr, size = ArrayPtr(duplicate)
+   case []float32:
+      if len(data) == 0 {
+         size = 0
+         ptr = gl.Pointer(nil)
+      } else {
+         var v float32
+         size = gl.Sizeiptr(len(data) * int(unsafe.Sizeof(v)))
+         ptr = gl.Pointer(&data[0])
+      }
+   case []gl.Float:
+      if len(data) == 0 {
+         size = 0
+         ptr = gl.Pointer(nil)
+      } else {
+         var v gl.Float
+         size = gl.Sizeiptr(len(data) * int(unsafe.Sizeof(v)))
+         ptr = gl.Pointer(&data[0])
+      }
+   case []int16:
+      if len(data) == 0 {
+         size = 0
+         ptr = gl.Pointer(nil)
+      } else {
+         var v int16
+         size = gl.Sizeiptr(len(data) * int(unsafe.Sizeof(v)))
+         ptr = gl.Pointer(&data[0])
+      }
+   case []gl.Ushort:
+      if len(data) == 0 {
+         size = 0
+         ptr = gl.Pointer(nil)
+      } else {
+         var v gl.Ushort
+         size = gl.Sizeiptr(len(data) * int(unsafe.Sizeof(v)))
+         ptr = gl.Pointer(&data[0])
+      }
+   case []int:
+      duplicate := make([]gl.Ushort, len(data))
+      for i, v := range data {
+         duplicate[i] = gl.Ushort(v)
+      }
+      ptr, size = ArrayPtr(duplicate)
+   case []gl.Int:
+      if len(data) == 0 {
+         size = 0
+         ptr = gl.Pointer(nil)
+      } else {
+         var v gl.Int
+         size = gl.Sizeiptr(len(data) * int(unsafe.Sizeof(v)))
+         ptr = gl.Pointer(&data[0])
+      }
+   default:
+      panic(fmt.Sprintln("unknown data type:", reflect.TypeOf(data)))
+   }
+   return ptr, size
 }
 
 func BindArrayData(buffer gl.Buffer, data interface{}) {
-    ptr, size := ArrayPtr(data)
-    gl.BindBuffer(gl.ARRAY_BUFFER, buffer)
-    gl.BufferData(gl.ARRAY_BUFFER, size, ptr, gl.STATIC_DRAW)
+   ptr, size := ArrayPtr(data)
+   gl.BindBuffer(gl.ARRAY_BUFFER, buffer)
+   gl.BufferData(gl.ARRAY_BUFFER, size, ptr, gl.STATIC_DRAW)
 }
 
 func ImageData(img image.Image) (gl.Sizei, gl.Sizei, gl.Enum, gl.Enum, gl.Pointer) {
-    switch img := img.(type) {
-    case *image.NRGBA:
-        return gl.Sizei(img.Rect.Dx()), gl.Sizei(img.Rect.Dy()), gl.RGBA, gl.UNSIGNED_BYTE, gl.Pointer(&img.Pix[0])
-    case *image.RGBA:
-        return gl.Sizei(img.Rect.Dx()), gl.Sizei(img.Rect.Dy()), gl.RGBA, gl.UNSIGNED_BYTE, gl.Pointer(&img.Pix[0])
-    default:
-        panic(reflect.TypeOf(img))
-    }
-    return 0,0,gl.RGB,gl.UNSIGNED_BYTE,nil
+   switch img := img.(type) {
+   case *image.NRGBA:
+      return gl.Sizei(img.Rect.Dx()), gl.Sizei(img.Rect.Dy()), gl.RGBA, gl.UNSIGNED_BYTE, gl.Pointer(&img.Pix[0])
+   case *image.RGBA:
+      return gl.Sizei(img.Rect.Dx()), gl.Sizei(img.Rect.Dy()), gl.RGBA, gl.UNSIGNED_BYTE, gl.Pointer(&img.Pix[0])
+   default:
+      panic(reflect.TypeOf(img))
+   }
+   return 0, 0, gl.RGB, gl.UNSIGNED_BYTE, nil
 }
 
 func LoadTexture(texture gl.Texture, filename string) error {
-    file, err := os.Open(filename)
-    if err != nil {
-        return err
-    }
-    defer file.Close()
-    img, _, err := image.Decode(file)
-    if err != nil {
-        return err
-    }
-    width, height, format, channelType, pixels := ImageData(img)
-    gl.BindTexture(gl.TEXTURE_2D, texture)
-    gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
-    gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
-    gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S,     gl.CLAMP_TO_EDGE)
-    gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T,     gl.CLAMP_TO_EDGE)
-    gl.TexImage2D(
-          gl.TEXTURE_2D, 0,           /* target, level of detail */
-          gl.RGB8,                    /* internal format */
-          width, height, 0,           /* width, height, border */
-          format, channelType,          /* external format, type */
-          pixels,                      /* pixels */
-    )
-    return nil
+   file, err := os.Open(filename)
+   if err != nil {
+      return err
+   }
+   defer file.Close()
+   img, _, err := image.Decode(file)
+   if err != nil {
+      return err
+   }
+   width, height, format, channelType, pixels := ImageData(img)
+   gl.BindTexture(gl.TEXTURE_2D, texture)
+   gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
+   gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
+   gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
+   gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
+   gl.TexImage2D(
+      gl.TEXTURE_2D, 0, /* target, level of detail */
+      gl.RGB8,          /* internal format */
+      width, height, 0, /* width, height, border */
+      format, channelType, /* external format, type */
+      pixels, /* pixels */
+   )
+   return nil
 }
 
 func LoadVertexShaderSource(shader gl.VertexShader, filename string) error {
-    return LoadShader(gl.Uint(shader), filename)
+   return LoadShader(gl.Uint(shader), filename)
 }
 
 func LoadFragmentShaderSource(shader gl.FragmentShader, filename string) error {
-    return LoadShader(gl.Uint(shader), filename)
+   return LoadShader(gl.Uint(shader), filename)
 }
 
 func LoadShader(shader gl.Uint, filename string) error {
-    bytes, err := ioutil.ReadFile(filename)
-    if err != nil {
-        return err
-    }
-    source := string(bytes)
-    gl.ShaderSource(shader, []string{source})
-    gl.CompileShader(shader)
-    var ok gl.Int
-    gl.GetShaderiv(shader, gl.COMPILE_STATUS, &ok)
-    if (ok == 0) {
-        fmt.Fprintln(os.Stderr, gl.GetShaderInfoLog(shader))
-        gl.DeleteShader(shader)
-        return errors.New("Failed to compile " + filename + "\n")
-    }
-    return nil
+   bytes, err := ioutil.ReadFile(filename)
+   if err != nil {
+      return err
+   }
+   source := string(bytes)
+   gl.ShaderSource(shader, []string{source})
+   gl.CompileShader(shader)
+   var ok gl.Int
+   gl.GetShaderiv(shader, gl.COMPILE_STATUS, &ok)
+   if ok == 0 {
+      fmt.Fprintln(os.Stderr, gl.GetShaderInfoLog(shader))
+      gl.DeleteShader(shader)
+      return errors.New("Failed to compile " + filename + "\n")
+   }
+   return nil
 }
 
 func LoadProgram(program gl.Program, vertexShader gl.VertexShader, fragmentShader gl.FragmentShader) error {
-    gl.AttachShader(program, gl.Uint(vertexShader))
-    gl.AttachShader(program, gl.Uint(fragmentShader))
-    gl.LinkProgram(program)
-    var ok gl.Int
-    gl.GetProgramiv(program, gl.LINK_STATUS, &ok)
-     if (ok == 0) {
-        fmt.Fprintln(os.Stderr, gl.GetProgramInfoLog(program))
-         gl.DeleteProgram(program)
-         return errors.New("Failed to link shader program")
-     }
-     return nil
+   gl.AttachShader(program, gl.Uint(vertexShader))
+   gl.AttachShader(program, gl.Uint(fragmentShader))
+   gl.LinkProgram(program)
+   var ok gl.Int
+   gl.GetProgramiv(program, gl.LINK_STATUS, &ok)
+   if ok == 0 {
+      fmt.Fprintln(os.Stderr, gl.GetProgramInfoLog(program))
+      gl.DeleteProgram(program)
+      return errors.New("Failed to link shader program")
+   }
+   return nil
 }
 
-var bufferType          = reflect.TypeOf(gl.Buffer(0))
-var textureType         = reflect.TypeOf(gl.Texture(0))
-var vaoType             = reflect.TypeOf(gl.VertexArrayObject(0))
-var vertexShaderType    = reflect.TypeOf(gl.VertexShader(0))
-var fragmentShaderType  = reflect.TypeOf(gl.FragmentShader(0))
-var programType         = reflect.TypeOf(gl.Program(0))
+var bufferType = reflect.TypeOf(gl.Buffer(0))
+var textureType = reflect.TypeOf(gl.Texture(0))
+var vaoType = reflect.TypeOf(gl.VertexArrayObject(0))
+var vertexShaderType = reflect.TypeOf(gl.VertexShader(0))
+var fragmentShaderType = reflect.TypeOf(gl.FragmentShader(0))
+var programType = reflect.TypeOf(gl.Program(0))
 var uniformLocationType = reflect.TypeOf(gl.UniformLocation(0))
 var attributeLocationType = reflect.TypeOf(gl.AttributeLocation(0))
 
+func Bind(bindings interface{}) {
+   value := reflect.ValueOf(bindings).Elem()
+   n := value.NumField()
 
-func Bind (bindings interface{}) {
-    value := reflect.ValueOf(bindings).Elem()
-    n := value.NumField()
-    
-    for i := 0; i < n; i++ {
-        field := value.Field(i)
-        switch field.Type() {
-        case bufferType: 
-            buffer := gl.GenBuffer()
-            field.Set(reflect.ValueOf(buffer))
-        case textureType:
-            texture := gl.GenTexture()
-            field.Set(reflect.ValueOf(texture))
-        case vaoType:
-            vao := gl.GenVertexArray()
-            field.Set(reflect.ValueOf(vao))
-        case vertexShaderType:
-            shader := gl.VertexShader(gl.CreateShader(gl.VERTEX_SHADER))
-            field.Set(reflect.ValueOf(shader))
-        case fragmentShaderType:
-            shader := gl.FragmentShader(gl.CreateShader(gl.FRAGMENT_SHADER))
-            field.Set(reflect.ValueOf(shader))
-        case programType:
-            program := gl.CreateProgram()
-            field.Set(reflect.ValueOf(program))
-        }   
-    }
-    // VertexBuffer gl.Buffer
-    // ElementBuffer gl.Buffer
-    // Tex [2]gl.Texture
-    // VAO gl.VertexArrayObject    
+   for i := 0; i < n; i++ {
+      field := value.Field(i)
+      switch field.Type() {
+      case bufferType:
+         buffer := gl.GenBuffer()
+         field.Set(reflect.ValueOf(buffer))
+      case textureType:
+         texture := gl.GenTexture()
+         field.Set(reflect.ValueOf(texture))
+      case vaoType:
+         vao := gl.GenVertexArray()
+         field.Set(reflect.ValueOf(vao))
+      case vertexShaderType:
+         shader := gl.VertexShader(gl.CreateShader(gl.VERTEX_SHADER))
+         field.Set(reflect.ValueOf(shader))
+      case fragmentShaderType:
+         shader := gl.FragmentShader(gl.CreateShader(gl.FRAGMENT_SHADER))
+         field.Set(reflect.ValueOf(shader))
+      case programType:
+         program := gl.CreateProgram()
+         field.Set(reflect.ValueOf(program))
+      }
+   }
 }
 
 func BindProgramLocations(program gl.Program, bindings interface{}) {
-    value := reflect.ValueOf(bindings).Elem()
-    n := value.NumField()
-    for i := 0; i < n; i++ {
-        field := value.Field(i)
-        name := value.Type().Field(i).Tag.Get("gl")
-        if name != "" {
-            switch field.Type() {
-            case uniformLocationType: 
-                location := gl.GetUniformLocation(program, name)
-                field.Set(reflect.ValueOf(location))
-                PanicOnError()
-            case attributeLocationType:
-                location := gl.GetAttribLocation(program, name)
-                field.Set(reflect.ValueOf(location))
-                PanicOnError()
-            }
-        }
-    }
+   value := reflect.ValueOf(bindings).Elem()
+   n := value.NumField()
+   for i := 0; i < n; i++ {
+      field := value.Field(i)
+      name := value.Type().Field(i).Tag.Get("gl")
+      if name != "" {
+         switch field.Type() {
+         case uniformLocationType:
+            location := gl.GetUniformLocation(program, name)
+            field.Set(reflect.ValueOf(location))
+            PanicOnError()
+         case attributeLocationType:
+            location := gl.GetAttribLocation(program, name)
+            field.Set(reflect.ValueOf(location))
+            PanicOnError()
+         }
+      }
+   }
 }
 
 func AttachTexture(location gl.UniformLocation, textureEnum gl.Enum, target gl.Enum, texture gl.Texture) {
-    gl.ActiveTexture(textureEnum)
-    gl.BindTexture(target, texture)
-    gl.Uniform1i(location, gl.Int(textureEnum - gl.TEXTURE0))
+   gl.ActiveTexture(textureEnum)
+   gl.BindTexture(target, texture)
+   gl.Uniform1i(location, gl.Int(textureEnum-gl.TEXTURE0))
 }
 
 func PanicOnError() {
-    err := gl.GetError()
-    if err != gl.NO_ERROR {
-        switch err {
-        case gl.INVALID_ENUM:
-            fmt.Println("INVALID_ENUM")
-        case gl.INVALID_VALUE:
-            fmt.Println("INVALID_VALUE")
-        case gl.INVALID_OPERATION:
-            fmt.Println("GL_INVALID_OPERATION")
-        case gl.INVALID_FRAMEBUFFER_OPERATION:
-            fmt.Println("GL_INVALID_FRAMEBUFFER_OPERATION")
-        case gl.OUT_OF_MEMORY:
-            fmt.Println("GL_OUT_OF_MEMORY")
-        default:
-            fmt.Println("other error", err)
-        }
-        debug.PrintStack()
-        panic(err)
-    }
+   err := gl.GetError()
+   if err != gl.NO_ERROR {
+      switch err {
+      case gl.INVALID_ENUM:
+         fmt.Println("INVALID_ENUM")
+      case gl.INVALID_VALUE:
+         fmt.Println("INVALID_VALUE")
+      case gl.INVALID_OPERATION:
+         fmt.Println("GL_INVALID_OPERATION")
+      case gl.INVALID_FRAMEBUFFER_OPERATION:
+         fmt.Println("GL_INVALID_FRAMEBUFFER_OPERATION")
+      case gl.OUT_OF_MEMORY:
+         fmt.Println("GL_OUT_OF_MEMORY")
+      default:
+         fmt.Println("other error", err)
+      }
+      debug.PrintStack()
+      panic(err)
+   }
 }
 
 type StencilOp struct {
@@ -353,12 +354,12 @@ func (s *StencilOp) Disable() *StencilOp {
 }
 
 func (s *StencilOp) Draw() *StencilOp {
-   gl.ColorMask(gl.TRUE, gl.TRUE, gl.TRUE, gl.TRUE)  
+   gl.ColorMask(gl.TRUE, gl.TRUE, gl.TRUE, gl.TRUE)
    return s
 }
 
 func (s *StencilOp) NoDraw() *StencilOp {
-   gl.ColorMask(gl.FALSE, gl.FALSE, gl.FALSE, gl.FALSE)  
+   gl.ColorMask(gl.FALSE, gl.FALSE, gl.FALSE, gl.FALSE)
    return s
 }
 
@@ -372,7 +373,7 @@ func (s *StencilOp) Unmask(level int) *StencilOp {
    return s
 }
 
-func (s *StencilOp) Depth()  *StencilOp {
+func (s *StencilOp) Depth() *StencilOp {
    gl.Enable(gl.DEPTH_TEST)
    return s
 }
